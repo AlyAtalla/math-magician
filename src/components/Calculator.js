3266923a46b40282aab22b3780abac78cfc263cb
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-07 16:23:08 +0300

    Fix linter errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,38 @@
 import React from 'react';
-import './Calculator.css'; // Import your component's CSS file
+import './Calculator.css';
 
 function Calculator() {
+  const handleClick = (value) => {
+    // Handle click logic here
+  };
+
   return (
     <div className="calculator">
       <div className="display">0</div>
       <div className="buttons">
         <div className="button-row">
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
-          <button>÷</button>
+          <button type="button" className="button" onClick={() => handleClick(7)}>7</button>
+          <button type="button" className="button" onClick={() => handleClick(8)}>8</button>
+          <button type="button" className="button" onClick={() => handleClick(9)}>9</button>
+          <button type="button" className="button" onClick={() => handleClick('÷')}>÷</button>
         </div>
         <div className="button-row">
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>x</button>
+          <button type="button" className="button" onClick={() => handleClick(4)}>4</button>
+          <button type="button" className="button" onClick={() => handleClick(5)}>5</button>
+          <button type="button" className="button" onClick={() => handleClick(6)}>6</button>
+          <button type="button" className="button" onClick={() => handleClick('x')}>x</button>
         </div>
         <div className="button-row">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>-</button>
+          <button type="button" className="button" onClick={() => handleClick(1)}>1</button>
+          <button type="button" className="button" onClick={() => handleClick(2)}>2</button>
+          <button type="button" className="button" onClick={() => handleClick(3)}>3</button>
+          <button type="button" className="button" onClick={() => handleClick('-')}>-</button>
         </div>
         <div className="button-row">
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
-          <button>+</button>
+          <button type="button" className="button" onClick={() => handleClick(0)}>0</button>
+          <button type="button" className="button" onClick={() => handleClick('.')}>.</button>
+          <button type="button" className="button" onClick={() => handleClick('=')}>=</button>
+          <button type="button" className="button" onClick={() => handleClick('+')}>+</button>
         </div>
       </div>
     </div>
