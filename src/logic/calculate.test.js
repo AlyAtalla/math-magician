aadfdfa3798cc324df6b589531190475790c08e2
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-14 15:10:40 +0300

    create Quotes, homepage, App page

--- src/logic/calculate.test.js
+++
@@ -1,9 +0,0 @@
-import calculate from './calculate';
-
-describe('calculate', () => {
-  it('resets the calculator object when AC button is pressed', () => {
-    const obj = { total: '3', next: '2', operation: '+' };
-    const buttonName = 'AC';
-    expect(calculate(obj, buttonName)).toEqual({ total: null, next: null, operation: null });
-  });
-});
