3e60179b67449ce11a737dfd621796d342ba5340
AlyAtalla <101482711+AlyAtalla@users.noreply.github.com>
2023-08-15 11:32:18 +0300

    Edit test files

--- src/components/Calculator.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Calculator from './Calculator';
-
-test('renders Calculator component correctly', () => {
-  const { asFragment } = render(<Calculator />);
-  expect(asFragment()).toMatchSnapshot();
-});
--- src/components/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Home from './Home';
-
-test('renders Home component correctly', () => {
-  const { asFragment } = render(<Home />);
-  expect(asFragment()).toMatchSnapshot();
-});
